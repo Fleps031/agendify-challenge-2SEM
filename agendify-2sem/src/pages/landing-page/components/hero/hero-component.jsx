a46804d929e018bab4749c057fef0a0872b12be8
Chariskyrim <enzolucianox@gmail.com>
2025-04-22 13:35:18 -0300

    revert

--- agendify-2sem/src/pages/landing-page/components/hero/hero-component.jsx
+++ agendify-2sem/src/pages/landing-page/components/hero/hero-component.jsx
@@ -1,9 +1,34 @@
 import './hero-component.css'
+import img01 from "../../../../images/carrossel/img01.jpg"
+import img02 from "../../../../images/carrossel/img02.jpg"
+import img03 from "../../../../images/carrossel/img03.jpg"
 
 export default function HeroComponent(){
     return(
         <>
-            <h1>Hero Component Works!</h1>
+            <div className="d-flex flex-column p-0 w-100 mt-150 container-fluid">
+                <div className="d-flex justify-content-center">
+                  <h1 className = 'align-self-end pb-2 fw-bold'>Agendify</h1>
+                  <h3 className = 'txt-roxo-custom ponto-grande'>.</h3>
+                  <h3 className = 'align-self-end pb-2 ms-3'>Organize e facilite a rotina dos seus pacientes</h3>
+                </div>
+
+                <div className = 'line-custom bg-roxo-custom mx-auto'></div>
+
+                <div className="d-flex justify-content-center mt-5">
+                    <h3>Serviços de fila e agendamento pra encaixar seu paciente no <span className = 'txt-roxo-custom'>momento</span> certo.</h3>
+                </div>
+
+                <div className="d-flex flex-column bg-black w-100 mt-5 h-100 p-4 justify-content-center align-items-center">
+                    <div className="mb-3 line-custom-ver2 bg-white"></div>
+                    <div className="d-flex gap-3 justify-content-center align-items-center">
+                      <img src={img01} alt="img01" className = 'scale-hover'/>
+                      <img src={img02} alt="img02" className = 'scale-hover'/>
+                      <img src={img03} alt="img03" className = 'scale-hover'/>
+                    </div>
+                    <div className="mt-3 line-custom-ver2 bg-white"></div>
+                </div>
+            </div>
         </>
     )
 }
