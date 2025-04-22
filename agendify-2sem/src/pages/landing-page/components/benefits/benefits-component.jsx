d77a951e917136ece872a72f19bb526661488bc5
alecoldelf <alecolvet248@gmail.com>
2025-04-22 17:41:34 -0300

    update

--- agendify-2sem/src/pages/landing-page/components/benefits/benefits-component.jsx
+++ agendify-2sem/src/pages/landing-page/components/benefits/benefits-component.jsx
@@ -3,7 +3,15 @@ import './benefits-component.css'
 export default function BenefitsComponent(){
     return(
         <>
-            <h1>Benefits Component Works!</h1>
+            <div className='d-flex container-fluid flex-column w-100'>
+                <div className='altura-card-beneficio d-flex justify-content-center align-items-center'>
+                    <h3 className='txt-roxo-custom text-center'>Por que escolher a Agendify?</h3>
+                </div>
+                <div className='d-flex bg-roxo-custom altura-card-beneficio'>
+                    <div className='line-custom-ver3 bg-white'></div>
+                        <p>Centenas de pacientes perdem agendamentos e consultas por indisponibilidade de horários e falta de visibilidade do tempo de espera e seu lugar na fila todos os anos</p>
+                </div>
+            </div>
         </>
     )
 }
