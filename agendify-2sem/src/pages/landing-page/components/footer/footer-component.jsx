ab0ec75fc23db1e460cec6010180aae0b9447bed
alecoldelf <alecolvet248@gmail.com>
2025-04-23 00:32:11 -0300

    uptade main screen

--- agendify-2sem/src/pages/landing-page/components/footer/footer-component.jsx
+++ agendify-2sem/src/pages/landing-page/components/footer/footer-component.jsx
@@ -3,7 +3,17 @@ import './footer-component.css'
 export default function FooterComponent(){
     return(
         <>
-            <h1>Footer Component Works!</h1>
+            <div className = 'd-flex mt-50 bg-black p-4'>
+                <div className = 'd-flex flex-column container-fluid justify-content-center'>
+                    <h1 className = 'text-white'>Agendify<span className = 'txt-roxo-custom'>.</span></h1>
+                    <p className = 'text-white fonte-pequena'>Copyright 2025 © Agendify. All Rights Reserved.</p>
+                </div>
+                <div className = 'd-flex flex-column container-fluid justify-content-center align-items-end'>
+                    <h5 className = 'text-white'>Entre em contato conosco!</h5>
+                    <p className = 'text-white'>suporte-agendify@gmail.com</p>
+                    <p className = 'text-white'>(11) 99 386-3819</p>
+                </div>
+            </div>
         </>
     )
 }
