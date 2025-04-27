9369a942791f06e9352142d5416edbb064b53596
Fleps031 <whaleluv3r@gmail.com>
2025-04-27 15:33:57 -0300

    'aaaa'

--- agendify-2sem/src/pages/system-demo/components/queue-dashboard/queue-dashboard.jsx
+++ agendify-2sem/src/pages/system-demo/components/queue-dashboard/queue-dashboard.jsx
@@ -1,63 +1,25 @@
 import { useEffect, useState } from 'react';
 import { FaTrashAlt } from "react-icons/fa";
 import './queue-dashboard.css';
+import * as faker from 'faker-br'
+import { cpf } from 'cpf-cnpj-validator';
+
+const knownStatus = ['Agendado', 'Aguardando confirmação', 'Em análise', 'Notificação enviada']
+
+
+const statusCoverage = {
+    3: 'Agendado',
+    6: 'Aguardando confirmação',
+    8: 'Notificação enviada',
+    9: 'Aguardando confirmação',
+}
 
 const statusColors = {
     'Agendado': 'status-booked',
-    'Aguardando confirmaÃ§Ã£o': 'status-awaiting',
-    'Em anÃ¡lise': 'status-processing',
-    'NotificaÃ§Ã£o enviada': 'status-notified',
+    'Aguardando confirmação': 'status-awaiting',
+    'Em análise': 'status-processing',
+    'Notificação enviada': 'status-notified',
 }
-
-const mockPacientes = [
-    {
-        name: 'Mark',
-        position: 'Grayson',
-        requestedAt: '12-03-2025',
-        status: 'Agendado'
-    },
-    {
-        name: 'Mark',
-        position: 'Grayson',
-        requestedAt: '12-03-2025',
-        status: 'Agendado'
-    },
-    {
-        id: '2',
-        name: 'Nolan',
-        position: 'Man',
-        requestedAt: '12-03-2025',
-        status: 'Aguardando confirmaÃ§Ã£o'
-    },
-    {
-        id: '2',
-        name: 'Livia',
-        position: 'Pereira',
-        requestedAt: '12-03-2025',
-        status: 'NotificaÃ§Ã£o enviada'
-    },
-    {
-        id: '2',
-        name: 'Livia',
-        position: 'Pereira',
-        requestedAt: '12-03-2025',
-        status: 'Em anÃ¡lise'
-    },
-    {
-        id: '2',
-        name: 'Livia',
-        position: 'Pereira',
-        requestedAt: '12-03-2025',
-        status: 'Em anÃ¡lise'
-    },
-    {
-        id: '2',
-        name: 'Livia',
-        position: 'Pereira',
-        requestedAt: '12-03-2025',
-        status: 'Em anÃ¡lise'
-    },
-]
 
 function DashboardTableRow({ patientRow, patientIndex }) {
     const statusClass = statusColors[patientRow?.status]
@@ -65,14 +27,16 @@ function DashboardTableRow({ patientRow, patientIndex }) {
     return (
         <>
             <tr className='text-center'>
-                <th scope="row">{patientIndex + 100}</th>
-                <td>{patientRow?.name}</td>
-                <td>{patientRow?.position}</td>
-                <td>{patientRow?.requestedAt}</td>
-                <td>
-                    <span className={'d-flex justify-content-center badge ' + statusClass}>{patientRow?.status}</span>
+                <th scope="row">{patientRow?.id}</th>
+                <td className='align-middle'>{patientIndex + 1 + 'º'}</td>
+                <td className='align-middle'>{patientRow?.document}</td>
+                <td className='align-middle'>{patientRow?.patientName}</td>
+                <td className='align-middle'>{patientRow?.relativeName}</td>
+                <td className='align-middle'>{patientRow?.requestedAt}</td>
+                <td className='align-middle'>
+                    <span className={'badge ' + statusClass}>{patientRow?.status}</span>
                 </td>
-                <td className='p-1'>
+                <td className='align-middle'>
                     <FaTrashAlt role='button' className='trash-can' />
                 </td>
             </tr>
@@ -81,30 +45,63 @@ function DashboardTableRow({ patientRow, patientIndex }) {
 }
 
 export default function QueueDashboard() {
-    const [person, setPerson] = useState();
-    let isFirstUseCall = true;
+    const [patients, setPatients] = useState([]);
+    const [initial, setInitial] = useState([0])
 
     useEffect(() => {
-        if (isFirstUseCall) {
-            createUsers(5);
-            isFirstUseCall = false;
-        }
+        createInitialPatients();
     }, [])
 
-    function createUsers(n) {
-        const url = 'https://randomuser.me/api/';
 
-        let promises = []
-        let results = []
+    function getStatusByPosition(pos){
+        for (let i in statusCoverage){
+            if(pos + 1 <= i){
+                return statusCoverage[i]
+            }
+        }
+    }
 
-        for (let i = 0; i <= n; i++) {
-            promises.push(setTimeout(() => {
-                fetch(url)
-            }, 1000))
+
+    function createInitialPatients(){
+        const newPatients = createPatients(10);
+        console.log(newPatients)
+        setPatients(newPatients);
+    }
+
+
+    function createPatients(n){
+        const newPatients = []
+        
+        for(let i=0; i<n; i++){
+            let pastDate = faker.date.past()
+            
+            let newDate = {
+                day: pastDate.getDate().toString(),
+                month: (pastDate.getMonth() +1).toString(),
+                year: '2024'
+            }
+
+            let familyName = faker.name.lastName()
+
+            newPatients.push(
+                {
+                    id: faker.random.uuid(),
+                    document: cpf.format(faker.br.cpf()),
+                    patientName: faker.name.firstName() + ' ' + familyName,
+                    relativeName: faker.name.firstName() + ' ' + familyName,
+                    status: getStatusByPosition(i),
+                    requestedAt: newDate.day + '/' + newDate.month + '/' + newDate.year
+                }
+            )
         }
 
-        Promise.all([...promises]).then((values) => { console.log(values) })
+        console.log(faker.date.past().getDate())
+        console.log(faker.date.recent().getFullYear())
+        console.log(faker.date.past().getMonth())
+        
+        return newPatients
     }
+
 
     return (
         <>
@@ -114,25 +111,27 @@ export default function QueueDashboard() {
                 </div>
 
                 <div className='row text-start mb-1'>
-                    <h2 className='fw-regular'>RequisiÃ§Ãµes de agendamento/encaixe</h2>
+                    <h2 className='fw-regular'>Requisições de agendamento/encaixe</h2>
                 </div>
 
                 <div className='row text-start space-between column-gap-5 ps-3'>
-                    <div className='col-md-8 p-0'>
-                        <div className='container-fluid overflow-auto p-0 table-responsive border-bottom-0 card rounded-1 p-0 m-0 '>
-                            <table className="table m-0">
-                                <thead className='text-center'>
+                    <div className='col-md-8 p-0 h-30'>
+                        <div className='container-fluid overflow-auto p-0 table-responsive border-bottom-0 card rounded-1 p-0 m-0 dash-max-height'>
+                            <table className="table m-0 fixedHeader">
+                                <thead className='text-center sticky-top'>
                                     <tr className='table-dark'>
-                                        <th scope='col text-center'>ID</th>
-                                        <th scope='col'>Nome do paciente</th>
-                                        <th scope='col'>PosiÃ§ao na fila</th>
-                                        <th scope='col'>Data da solicitaÃ§Ã£o</th>
-                                        <th scope='col'>Status</th>
-                                        <th scope='col'>AÃ§Ãµes</th>
+                                        <th className='align-middle' scope='col text-center'>ID</th>
+                                        <th className='align-middle' scope='col'>Posição na fila</th>
+                                        <th className='align-middle' scope='col'>Documento (CPF)</th>
+                                        <th className='align-middle' scope='col'>Nome do paciente</th>
+                                        <th className='align-middle' scope='col'>Nome do Responsável</th>
+                                        <th className='align-middle' scope='col'>Data da solicitação</th>
+                                        <th className='align-middle' scope='col'>Status</th>
+                                        <th className='align-middle' scope='col'>Ações</th>
                                     </tr>
                                 </thead>
                                 <tbody>
-                                    {mockPacientes.map((el, idx) => <DashboardTableRow patientRow={el} patientIndex={idx} key={idx} />)}
+                                    {patients.map((el, idx) => <DashboardTableRow patientRow={el} patientIndex={idx} key={idx} />)}
                                 </tbody>
                             </table>
                         </div>
