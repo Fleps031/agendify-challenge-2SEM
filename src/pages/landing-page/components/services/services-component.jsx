0d927dc15504e89ffdf4370f0aa0522c9c4e4e28
Fleps031 <whaleluv3r@gmail.com>
2025-06-12 20:44:58 -0300

    cleaning services css

--- src/pages/landing-page/components/services/services-component.jsx
+++ src/pages/landing-page/components/services/services-component.jsx
@@ -6,25 +6,25 @@ import card03 from "../../../../images/services/card03.png"
 export default function ServicesComponent(){
     return(
         <>
-            <div id='servicos' className = 'd-flex flex-column container-fluid mt-100 redimensionamento'>
+            <div id='servicos' className = 'd-flex flex-column container-fluid mt-100-px redimensionamento'>
                 <div className="d-flex flex-column align-items-center cards-redimensionamento">
                     <h1>Nossos serviços</h1>
-                    <div className = 'line-custom bg-roxo-custom'></div>
+                    <div className='agendify-divider background-agendify divider-small'></div>
                 </div>
                 <div className = 'w-100'>
                     <div className = 'd-flex flex-column mt-5 gap-4 align-items-center'>
                         <div className = 'd-flex w-75 align-items-center space-between gap-4 rounded borda scale-hover sombra redimensionamento'>
-                            <img src={card01} alt="card01" className = 'w-150 m-4'/>
+                            <img src={card01} alt="card01" className = 'w-150-px m-4'/>
                             <h1 className = 'fs-5 w-100 text-end pe-3'>Sistema integrado com as suas consultas e agendamentos, sem necessidade de migração</h1>
                         </div>
 
                         <div className = 'd-flex w-75 align-items-center space-between gap-4 rounded borda scale-hover sombra redimensionamento'>
-                            <img src={card02} alt="card01" className = 'w-150 m-4'/>
+                            <img src={card02} alt="card01" className = 'w-150-px m-4'/>
                             <h1 className = 'fs-5 w-100 text-end pe-3'>Encaixe seus pacientes em filas de espera de consultas e exames de forma inteligente</h1>
                         </div>
 
                         <div className = 'd-flex w-75 align-items-center space-between gap-4 rounded borda scale-hover sombra redimensionamento'>
-                            <img src={card03} alt="card01" className = 'w-150 m-4'/>
+                            <img src={card03} alt="card01" className = 'w-150-px m-4'/>
                             <h1 className = 'fs-5 w-100 text-end pe-3'>Disponibilizamos API e plataforma web pra te impulsionar sem perda de tempo</h1>
                         </div>
                     </div>
