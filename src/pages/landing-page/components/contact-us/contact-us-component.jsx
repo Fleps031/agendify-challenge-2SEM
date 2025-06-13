c876f1d0ee80217b54ecd6e2abf9d548fb83a9db
Fleps031 <whaleluv3r@gmail.com>
2025-06-12 21:49:54 -0300

    cleaning contact css

--- src/pages/landing-page/components/contact-us/contact-us-component.jsx
+++ src/pages/landing-page/components/contact-us/contact-us-component.jsx
@@ -5,16 +5,16 @@ import { Link } from "react-router";
 export default function ContactUsComponent(){
     return(
       <>
-        <div id='contato' className="row text-start space-between mt-4">
-          <div className="d-flex col-md-4 justify-content-center">
+        <div id='contato' className="row text-start space-between mt-4 p-4 flex-wrap">
+          <div className="d-flex col justify-content-center quebra-foto">
             <img className="largura-foto-contato" src={contactPhoto} alt="Contato" />
           </div>
 
-          <div className="col-md" >
+          <div className="col quebra-form h-100" >
             <h2 className="row titulo-contato justify-content-center text-center mt-2">Junte-se a nós, e garanta mais transparência e agilidade para os seus pacientes.</h2>
 
-            <div className = 'row altura-box ps-4 pe-4 largura-form mx-auto'>
-              <div className="d-flex flex-column borda sombra w-100 h-100 gap-4 mt-3 p-3 align-items-center justify-content-center">
+            <div className='row altura-box largura-form mx-auto align-items-center justify-content-center'>
+              <div className="d-flex flex-column borda sombra w-75 h-100 gap-4 mt-3 p-3 align-items-center justify-content-center">
                 <input className="form-custom" placeholder="E-mail" type="text" />
                 <input className="form-custom" placeholder="CPF/CNPJ" type="text" />
                 <input className="form-custom" placeholder="Número de telefone" type="text" />
@@ -25,7 +25,7 @@ export default function ContactUsComponent(){
             <div className = 'row'> 
               <div className="d-flex flex-column mt-5 justify-content-center w-75 mx-auto">
                 <h2 className="titulo-login text-center">Já possui cadastro? Faça login e acesse o sistema!</h2>
-                <Link role='button' className = 'text-decoration-none text-center align-self-center btn-custom mt-2 largura-botao' to="/sistema">Fazer login!</Link>
+                <Link role='button' className = 'text-decoration-none text-center align-self-center btn-custom' to="/sistema">Fazer login!</Link>
               </div>
             </div> 
           </div>
